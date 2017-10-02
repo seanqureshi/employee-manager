6733be60396b68b87a3518e6c7f78fecdc4ae0fd
seanqureshi <sean.m.qureshi@gmail.com>
2017-10-02 17:12:43 -0600

    update index.js to stage 5

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
 import App from './Stage 5/App';
-=======
-import App from './Stage 4/App';
->>>>>>> b5df7cd47e092fb60e7aa741101f50a1f80d7520
 import './index.css';
 
 ReactDOM.render(
